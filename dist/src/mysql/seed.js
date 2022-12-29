18cc59add85a72d58b7fc22fd2179f9634c4b79e
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-12-29 15:46:37 +0100

    seed js finished

--- dist/src/mysql/seed.js
+++ dist/src/mysql/seed.js
@@ -15,8 +15,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const connection_1 = require("./connection");
 const connection_2 = __importDefault(require("./connection"));
 const faker_1 = require("@faker-js/faker");
+const rooms = [];
+const users = [];
+const bookings = [];
+const contacts = [];
 connection_2.default.connect();
-Promise.all([createRoomsTable(), createUsersTable(), insertRooms(20), insertUsers(20)]).then(() => connection_2.default.end());
+Promise.all([
+    createRoomsTable(),
+    createUsersTable(),
+    createBookingsTable(),
+    createContactsTable()
+])
+    .then(() => insertRooms(20))
+    .then(() => insertUsers(20))
+    .then(() => insertBookings(20))
+    .then(() => insertContacts(20))
+    .then(() => connection_2.default.end());
 function createRoomsTable() {
     return __awaiter(this, void 0, void 0, function* () {
         yield (0, connection_1.dbQuery)("CREATE OR REPLACE TABLE rooms ( idroom VARCHAR(255), number SMALLINT, photo VARCHAR(500), type VARCHAR(255), amenities VARCHAR(500), price INT, offer INT, status VARCHAR(5), PRIMARY KEY(idroom));", null);
@@ -27,10 +41,21 @@ function createUsersTable() {
         yield (0, connection_1.dbQuery)("CREATE OR REPLACE TABLE users (iduser VARCHAR(255), name VARCHAR(255), photo VARCHAR(500), position VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), date VARCHAR(100), description VARCHAR(500), state VARCHAR(5), pass VARCHAR(255), PRIMARY KEY(iduser));", null);
     });
 }
+function createBookingsTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0, connection_1.dbQuery)("CREATE OR REPLACE TABLE bookings (idbooking VARCHAR(255), name VARCHAR(255), bookingorder VARCHAR(100), checkin VARCHAR(100), checkout VARCHAR(100), typeroom VARCHAR(255), numroom INT, price INT, request VARCHAR(255), amenities VARCHAR(255), photos VARCHAR(500), type VARCHAR(255), description VARCHAR(500), state VARCHAR(10), PRIMARY KEY(idbooking));", null);
+    });
+}
+function createContactsTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0, connection_1.dbQuery)("CREATE OR REPLACE TABLE contacts (idcontact VARCHAR(255), date VARCHAR(255), customer VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), header VARCHAR(255), comment VARCHAR(500), PRIMARY KEY(idcontact));", null);
+    });
+}
 function insertRooms(number) {
     return __awaiter(this, void 0, void 0, function* () {
         for (let i = 0; i < number; i++) {
             const room = setRandomRoom();
+            rooms.push(room);
             yield (0, connection_1.dbQuery)("INSERT INTO rooms SET ?", room);
         }
     });
@@ -39,7 +64,26 @@ function insertUsers(number) {
     return __awaiter(this, void 0, void 0, function* () {
         for (let i = 0; i < number; i++) {
             const user = setRandomUser();
+            users.push(user);
             yield (0, connection_1.dbQuery)("INSERT INTO users SET ?", user);
+        }
+    });
+}
+function insertBookings(number) {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let i = 0; i < number; i++) {
+            const booking = setRandomBooking();
+            bookings.push(booking);
+            yield (0, connection_1.dbQuery)("INSERT INTO bookings SET ?", booking);
+        }
+    });
+}
+function insertContacts(number) {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let i = 0; i < number; i++) {
+            const contact = setRandomContact();
+            contacts.push(contact);
+            yield (0, connection_1.dbQuery)("INSERT INTO contacts SET ?", contact);
         }
     });
 }
@@ -67,6 +111,37 @@ function setRandomUser() {
         description: faker_1.faker.random.words(30),
         state: String(faker_1.faker.datatype.boolean()),
         pass: faker_1.faker.internet.password()
+    };
+}
+function setRandomBooking() {
+    const room = rooms[Math.round(Math.random() * rooms.length - 1)];
+    const user = users[Math.round(Math.random() * rooms.length - 1)];
+    return {
+        idbooking: faker_1.faker.datatype.uuid(),
+        name: user.name,
+        bookingorder: String(faker_1.faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        checkin: String(faker_1.faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        checkout: String(faker_1.faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        typeroom: room.type,
+        numroom: room.number,
+        price: room.price,
+        request: faker_1.faker.random.words(3),
+        amenities: room.amenities,
+        photos: room.photo,
+        type: faker_1.faker.random.words(3),
+        description: faker_1.faker.random.words(30),
+        state: String(faker_1.faker.datatype.boolean())
+    };
+}
+function setRandomContact() {
+    return {
+        idcontact: faker_1.faker.datatype.uuid(),
+        date: String(faker_1.faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        customer: faker_1.faker.name.fullName(),
+        email: faker_1.faker.internet.email(),
+        phone: faker_1.faker.phone.number(),
+        header: faker_1.faker.random.words(5),
+        comment: faker_1.faker.random.words(30)
     };
 }
 //# sourceMappingURL=seed.js.map
--- src/mysql/seed.ts
+++ src/mysql/seed.ts
@@ -3,21 +3,42 @@ import connection from './connection';
 import { faker } from '@faker-js/faker';
 
 
+const rooms = [];
+const users = [];
+const bookings = [];
+const contacts = [];
+
 connection.connect();
-Promise.all([createRoomsTable(), createUsersTable(), insertRooms(20), insertUsers(20)])
+
+Promise.all([
+    createRoomsTable(),
+    createUsersTable(),
+    createBookingsTable(),
+    createContactsTable()])
+    .then(() => insertRooms(20))
+    .then(() => insertUsers(20))
+    .then(() => insertBookings(20))
+    .then(() => insertContacts(20))
     .then(() => connection.end());
 
 async function createRoomsTable() {
     await dbQuery("CREATE OR REPLACE TABLE rooms ( idroom VARCHAR(255), number SMALLINT, photo VARCHAR(500), type VARCHAR(255), amenities VARCHAR(500), price INT, offer INT, status VARCHAR(5), PRIMARY KEY(idroom));", null);
 }
-
 async function createUsersTable() {
     await dbQuery("CREATE OR REPLACE TABLE users (iduser VARCHAR(255), name VARCHAR(255), photo VARCHAR(500), position VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), date VARCHAR(100), description VARCHAR(500), state VARCHAR(5), pass VARCHAR(255), PRIMARY KEY(iduser));", null);
 }
+async function createBookingsTable() {
+    await dbQuery("CREATE OR REPLACE TABLE bookings (idbooking VARCHAR(255), name VARCHAR(255), bookingorder VARCHAR(100), checkin VARCHAR(100), checkout VARCHAR(100), typeroom VARCHAR(255), numroom INT, price INT, request VARCHAR(255), amenities VARCHAR(255), photos VARCHAR(500), type VARCHAR(255), description VARCHAR(500), state VARCHAR(10), PRIMARY KEY(idbooking));", null);
+}
+async function createContactsTable() {
+    await dbQuery("CREATE OR REPLACE TABLE contacts (idcontact VARCHAR(255), date VARCHAR(255), customer VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), header VARCHAR(255), comment VARCHAR(500), PRIMARY KEY(idcontact));", null);
+}
+
 
 async function insertRooms(number: number): Promise<void> {
     for (let i = 0; i < number; i++) {
         const room = setRandomRoom();
+        rooms.push(room);
         await dbQuery("INSERT INTO rooms SET ?", room);
     }
 }
@@ -25,7 +46,24 @@ async function insertRooms(number: number): Promise<void> {
 async function insertUsers(number: number): Promise<void> {
     for (let i = 0; i < number; i++) {
         const user = setRandomUser();
+        users.push(user);
         await dbQuery("INSERT INTO users SET ?", user);
+    }
+}
+
+async function insertBookings(number: number): Promise<void> {
+    for (let i = 0; i < number; i++) {
+        const booking = setRandomBooking();
+        bookings.push(booking);
+        await dbQuery("INSERT INTO bookings SET ?", booking);
+    }
+}
+
+async function insertContacts(number: number): Promise<void> {
+    for (let i = 0; i < number; i++) {
+        const contact = setRandomContact();
+        contacts.push(contact);
+        await dbQuery("INSERT INTO contacts SET ?", contact);
     }
 }
 
@@ -54,6 +92,41 @@ function setRandomUser() {
         description: faker.random.words(30),
         state: String(faker.datatype.boolean()),
         pass: faker.internet.password()
+    }
+}
+
+function setRandomBooking() {
+
+    const room = rooms[Math.round(Math.random() * rooms.length - 1)];
+    const user = users[Math.round(Math.random() * rooms.length - 1)];
+
+    return {
+        idbooking: faker.datatype.uuid(),
+        name: user.name,
+        bookingorder: String(faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        checkin: String(faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        checkout: String(faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        typeroom: room.type,
+        numroom: room.number,
+        price: room.price,
+        request: faker.random.words(3),
+        amenities: room.amenities,
+        photos: room.photo,
+        type: faker.random.words(3),
+        description: faker.random.words(30),
+        state: String(faker.datatype.boolean())
+    }
+}
+
+function setRandomContact() {
+    return {
+        idcontact: faker.datatype.uuid(),
+        date: String(faker.date.between('2021-01-01T00:00:00.000Z', '2022-12-01T00:00:00.000Z')),
+        customer: faker.name.fullName(),
+        email: faker.internet.email(),
+        phone: faker.phone.number(),
+        header: faker.random.words(5),
+        comment: faker.random.words(30)
     }
 }
 
