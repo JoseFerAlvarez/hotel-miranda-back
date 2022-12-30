b2452d9fcfb89970a60116cd324298db79953504
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-12-30 17:06:52 +0100

    sql file updated

--- querys.sql
+++ querys.sql
@@ -7,10 +7,55 @@ SHOW TABLES;
 
 
 /* Create tables*/
-CREATE OR REPLACE TABLE rooms (idroom INT NOT NULL AUTO_INCREMENT, number SMALLINT, photo VARCHAR(500), type VARCHAR(255), amenities VARCHAR(500), price INT, offer INT, status VARCHAR(5), PRIMARY KEY(idroom));
-CREATE OR REPLACE TABLE users (iduser INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), photo VARCHAR(500), position VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), date VARCHAR(100), description VARCHAR(500), state VARCHAR(5), pass VARCHAR(255), PRIMARY KEY(iduser));
-CREATE OR REPLACE TABLE bookings (idbooking INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), bookingorder VARCHAR(100), checkin VARCHAR(100), checkout VARCHAR(100), typeroom VARCHAR(255), numroom INT, price INT, request VARCHAR(255), amenities VARCHAR(255), photos VARCHAR(500), type VARCHAR(255), description VARCHAR(500), state VARCHAR(10), PRIMARY KEY(idbooking));
-CREATE OR REPLACE TABLE contacts (idcontact INT NOT NULL AUTO_INCREMENT, date VARCHAR(255), customer VARCHAR(255), email VARCHAR(255), phone VARCHAR(50), header VARCHAR(255), comment VARCHAR(500), PRIMARY KEY(idcontact));
+CREATE OR REPLACE TABLE rooms (
+idroom INT NOT NULL AUTO_INCREMENT,
+numroom SMALLINT,
+photo VARCHAR(500),
+type TINYINT,
+amenities VARCHAR(500),
+price INT,
+offer INT,
+status TINYINT,
+PRIMARY KEY (idroom));
+
+CREATE OR REPLACE TABLE users (
+iduser INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(255),
+photo VARCHAR(500),
+position VARCHAR(255),
+email VARCHAR(255),
+phone VARCHAR(50),
+date VARCHAR(100),
+description VARCHAR(500),
+state TINYINT,
+pass VARCHAR(255),
+PRIMARY KEY (iduser));
+
+CREATE OR REPLACE TABLE bookings (
+idbooking INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(255),
+bookingorder VARCHAR(100),
+checkin VARCHAR(100),
+checkout VARCHAR(100),
+type TINYINT,
+numroom SMALLINT,
+price INT,
+request VARCHAR(255),
+amenities VARCHAR(500),
+photos VARCHAR(500),
+description VARCHAR(500),
+state TINYINT,
+PRIMARY KEY (idbooking));
+
+CREATE OR REPLACE TABLE contacts (
+idcontact INT NOT NULL AUTO_INCREMENT,
+date VARCHAR(255),
+customer VARCHAR(255),
+email VARCHAR(255),
+phone VARCHAR(50),
+header VARCHAR(255),
+comment VARCHAR(500),
+PRIMARY KEY (idcontact));
 
 /* Drop table */
 DROP TABLE IF EXISTS table_name;
