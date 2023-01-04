7caa32e0ac44399534802d7e2b7b000ab43dfafd
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2023-01-04 13:44:35 +0100

    contacts tests created

--- dist/tests/contacts.test.js
+++ dist/tests/contacts.test.js
@@ -15,72 +15,112 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const index_1 = __importDefault(require("../src/index"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const connection_1 = require("../src/db/connection");
+const schemas_1 = require("../src/Schemas/schemas");
+const mongoose_1 = __importDefault(require("mongoose"));
 const token = jsonwebtoken_1.default.sign({ user: { _id: 1, email: process.env.DEFAULT_USER } }, process.env.SECRET_TOKEN);
+const id = "888888888888888888888888";
+const contact = {
+    _id: new mongoose_1.default.mongo.ObjectId("888888888888888888888888"),
+    date: "2020-09-08T06:25:19.082Z",
+    customer: 'Manolo Manolez',
+    email: 'Kane.Medhurst21@gmail.com',
+    phone: '+34 687 92 85 91',
+    header: 'Aperiam quae consequatur voluptatum nostrum iusto est nam.',
+    comment: 'Accusantium voluptatibus dolorem eligendi.',
+};
+beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, connection_1.connect)();
+}));
+afterAll(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, connection_1.disconnect)();
+    yield index_1.default.close();
+}));
 describe("Get contact list", () => {
     test("Get contacts without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default).get("/contacts");
-        expect(res.statusCode).toBe(401);
+        yield (0, supertest_1.default)(index_1.default)
+            .get("/contacts")
+            .expect(401);
     }));
     test("Get contacts with token", () => __awaiter(void 0, void 0, void 0, function* () {
+        const contacts = yield schemas_1.Contact.find().exec();
+        const contactsDbIds = contacts.map((contact) => {
+            return contact._id.toString();
+        });
         const res = yield (0, supertest_1.default)(index_1.default)
             .get("/contacts")
             .set("Authorization", "Bearer " + token);
-        expect(res.statusCode).toBe(200);
-    }));
-});
-describe("Get contact details", () => {
-    test("Get contact details without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default).get("/contacts/3");
-        expect(res.statusCode).toBe(401);
-    }));
-    test("Get contact details with token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default)
-            .get("/contacts/3")
-            .set("Authorization", "Bearer " + token);
+        const contactsRequestIds = res.body.map((contact) => {
+            return contact._id.toString();
+        });
+        expect(contactsRequestIds).toEqual(contactsDbIds);
         expect(res.statusCode).toBe(200);
     }));
 });
 describe("Post contact", () => {
     test("Post contact without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default)
+        yield (0, supertest_1.default)(index_1.default)
             .post("/contacts")
-            .send({});
-        expect(res.statusCode).toBe(401);
+            .expect(401);
     }));
     test("Post contact with token", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(index_1.default)
-            .get("/contacts")
+            .post("/contacts")
             .set("Authorization", "Bearer " + token)
-            .send({});
-        expect(res.statusCode).toBe(200);
+            .send({
+            contact: contact
+        })
+            .expect(200);
+        expect(res.body.newcontact._id).toEqual(id);
+    }));
+});
+describe("Get contact details", () => {
+    test("Get contact details without token", () => __awaiter(void 0, void 0, void 0, function* () {
+        yield (0, supertest_1.default)(index_1.default)
+            .get(`/contacts/${id}`)
+            .expect(401);
+    }));
+    test("Get contact details with token", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(index_1.default)
+            .get(`/contacts/${id}`)
+            .set("Authorization", "Bearer " + token)
+            .expect(200);
+        expect(res.body._id).toEqual(id);
     }));
 });
 describe("Put contact", () => {
     test("Put contact without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default)
-            .put("/contacts/3")
-            .send({});
-        expect(res.statusCode).toBe(401);
+        yield (0, supertest_1.default)(index_1.default)
+            .put(`/contacts/${id}`)
+            .send({
+            contact: contact
+        })
+            .expect(401);
     }));
     test("Put contact with token", () => __awaiter(void 0, void 0, void 0, function* () {
+        contact.customer = "Fernando Fernandez";
         const res = yield (0, supertest_1.default)(index_1.default)
-            .put("/contacts/3")
+            .put(`/contacts/${id}`)
             .set("Authorization", "Bearer " + token)
-            .send({});
+            .send({
+            contact: contact
+        });
+        expect(res.body.oldcontact.customer).not.toEqual(res.body.newcontact.customer);
         expect(res.statusCode).toBe(200);
     }));
 });
 describe("Delete contact", () => {
     test("Delete contact without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield (0, supertest_1.default)(index_1.default)
-            .delete("/contacts/3");
-        expect(res.statusCode).toBe(401);
+        yield (0, supertest_1.default)(index_1.default)
+            .delete(`/contacts/${id}`)
+            .expect(401);
     }));
     test("Delete contact with token", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(index_1.default)
-            .delete("/contacts/3")
-            .set("Authorization", "Bearer " + token);
-        expect(res.statusCode).toBe(200);
+            .delete(`/contacts/${id}`)
+            .set("Authorization", "Bearer " + token)
+            .expect(200);
+        expect(res.body.oldcontact._id).toEqual(id);
     }));
 });
 //# sourceMappingURL=contacts.test.js.map
--- tests/contacts.test.ts
+++ tests/contacts.test.ts
@@ -1,90 +1,133 @@
 import request from "supertest";
 import server from "../src/index";
 import jwt from "jsonwebtoken";
+import { connect, disconnect } from "../src/db/connection";
+import { Contact } from "../src/Schemas/schemas";
+import mongoose from "mongoose";
 
 const token: string = jwt.sign({ user: { _id: 1, email: process.env.DEFAULT_USER } }, process.env.SECRET_TOKEN);
 
+const id = "888888888888888888888888";
+const contact = {
+    _id: new mongoose.mongo.ObjectId("888888888888888888888888"),
+    date: "2020-09-08T06:25:19.082Z",
+    customer: 'Manolo Manolez',
+    email: 'Kane.Medhurst21@gmail.com',
+    phone: '+34 687 92 85 91',
+    header: 'Aperiam quae consequatur voluptatum nostrum iusto est nam.',
+    comment: 'Accusantium voluptatibus dolorem eligendi.',
+}
+
+beforeAll(async () => {
+    await connect();
+});
+
+afterAll(async () => {
+    await disconnect();
+    await server.close();
+});
+
 describe("Get contact list", (): void => {
     test("Get contacts without token", async (): Promise<void> => {
-        const res = await request(server).get("/contacts");
-        expect(res.statusCode).toBe(401);
+        await request(server)
+            .get("/contacts")
+            .expect(401);
     })
 
     test("Get contacts with token", async (): Promise<void> => {
+        const contacts = await Contact.find().exec();
+        const contactsDbIds = contacts.map((contact) => {
+            return contact._id.toString();
+        });
+
         const res = await request(server)
             .get("/contacts")
             .set("Authorization", "Bearer " + token);
 
-        expect(res.statusCode).toBe(200);
-    })
-});
+        const contactsRequestIds = res.body.map((contact) => {
+            return contact._id.toString();
+        });
 
-describe("Get contact details", (): void => {
-    test("Get contact details without token", async (): Promise<void> => {
-        const res = await request(server).get("/contacts/3");
-        expect(res.statusCode).toBe(401);
-    });
-
-    test("Get contact details with token", async (): Promise<void> => {
-        const res = await request(server)
-            .get("/contacts/3")
-            .set("Authorization", "Bearer " + token);
-
+        expect(contactsRequestIds).toEqual(contactsDbIds);
         expect(res.statusCode).toBe(200);
     })
 });
 
 describe("Post contact", (): void => {
     test("Post contact without token", async (): Promise<void> => {
-        const res = await request(server)
+        await request(server)
             .post("/contacts")
-            .send({});
-
-        expect(res.statusCode).toBe(401);
+            .expect(401);
     });
 
     test("Post contact with token", async (): Promise<void> => {
         const res = await request(server)
-            .get("/contacts")
+            .post("/contacts")
             .set("Authorization", "Bearer " + token)
-            .send({});
+            .send({
+                contact: contact
+            })
+            .expect(200);
 
-        expect(res.statusCode).toBe(200);
+        expect(res.body.newcontact._id).toEqual(id);
+    })
+});
+
+describe("Get contact details", (): void => {
+    test("Get contact details without token", async (): Promise<void> => {
+        await request(server)
+            .get(`/contacts/${id}`)
+            .expect(401);
+    });
+
+    test("Get contact details with token", async (): Promise<void> => {
+        const res = await request(server)
+            .get(`/contacts/${id}`)
+            .set("Authorization", "Bearer " + token)
+            .expect(200);
+
+        expect(res.body._id).toEqual(id);
     })
 });
 
 describe("Put contact", (): void => {
     test("Put contact without token", async (): Promise<void> => {
-        const res = await request(server)
-            .put("/contacts/3")
-            .send({});
-
-        expect(res.statusCode).toBe(401);
+        await request(server)
+            .put(`/contacts/${id}`)
+            .send({
+                contact: contact
+            })
+            .expect(401);
     });
 
     test("Put contact with token", async (): Promise<void> => {
-        const res = await request(server)
-            .put("/contacts/3")
-            .set("Authorization", "Bearer " + token)
-            .send({});
+        contact.customer = "Fernando Fernandez";
 
+        const res = await request(server)
+            .put(`/contacts/${id}`)
+            .set("Authorization", "Bearer " + token)
+            .send({
+                contact: contact
+            });
+
+        expect(res.body.oldcontact.customer).not.toEqual(res.body.newcontact.customer);
         expect(res.statusCode).toBe(200);
     })
 });
 
 describe("Delete contact", (): void => {
     test("Delete contact without token", async (): Promise<void> => {
-        const res = await request(server)
-            .delete("/contacts/3");
-
-        expect(res.statusCode).toBe(401);
+        await request(server)
+            .delete(`/contacts/${id}`)
+            .expect(401);
     });
 
     test("Delete contact with token", async (): Promise<void> => {
         const res = await request(server)
-            .delete("/contacts/3")
-            .set("Authorization", "Bearer " + token);
+            .delete(`/contacts/${id}`)
+            .set("Authorization", "Bearer " + token)
+            .expect(200);
 
-        expect(res.statusCode).toBe(200);
+        expect(res.body.oldcontact._id).toEqual(id);
     })
 });
